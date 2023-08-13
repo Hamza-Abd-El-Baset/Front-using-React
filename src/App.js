bf236816e7df207dd5e6654cf3648c174cd8d30c
Hamza Abd El-Baset <hamza.abdelbaset.elgebaly@gmail.com>
2023-08-13 11:22:24 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [user, setUser] = useState(null)
+  const login = ({username, password}) => {
+    axios.post(process.env.REACT_APP_BACKEND_URI + "/users/login", {
+        username,
+        password
+    })
+    .then(res => {
+      setUser(res.data.user)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }
+
+  const onFormSubmit = (e) => {
+    e.preventDefault()
+    const {
+      username: {value: username},
+      password: {value: password}
+    } = e.target
+    console.log(username)
+    console.log(password)
+    
+    login({username, password})
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={onFormSubmit}>
+          <h2>User Info</h2>
+          <input name="username" type="text" placeholder="Username"/>
+          <input name="password" type="password" placeholder="Password"/>
+          <input type="submit" value="Login"/>     
+        </form>
+
+
+        { user && (
+            <div>
+            <br/>
+            <label>Username</label>: {user.username}
+            <br/>
+            <label>Age</label>: {user.age}
+          </div>
+          )
+        }
+        
+
       </header>
     </div>
   );
